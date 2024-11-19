bde8199b93da2d8d8a2e3041f7f6c2efdc35bfbf
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-19 11:43:28 +0200

    add class DoFiles and fields

---
+++ labaexceptiontask1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace labaexceptiontask1
+{
+    class DoFiles
+    {
+        private List<string> files;
+        private List<string> nofiles;
+        private List<string> badData;
+        private List<string> overflowData;
+    }
+    internal class Program
+    {
+        static void Main()
+        { 
+        
+        }
+    }
+}

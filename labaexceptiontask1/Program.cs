c75c7a3b869795c8dcc6475c50cb0a3c45e94587
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-19 11:48:39 +0200

    add properties

--- labaexceptiontask1/Program.cs
+++ labaexceptiontask1/Program.cs
@@ -8,6 +8,22 @@ namespace labaexceptiontask1
         private List<string> nofiles;
         private List<string> badData;
         private List<string> overflowData;
+        public List<string> Files
+        {
+            get { return files; }
+        }
+        public List<string> NoFiles
+        {
+            get { return nofiles; }
+        }
+        public List<string> BadData
+        {
+            get { return badData; }
+        }
+        public List<string> OverflowData
+        {
+            get { return overflowData; }
+        }
     }
     internal class Program
     {
